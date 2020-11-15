26a344b52f50dc528e8c9e2b84f06a78469f55c0
askalag <askalag@gmail.com>
2020-11-15 19:48:16 +0300

    commit

---
+++ Comparator/ObjComparator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqApp.Model;
+
+namespace LinqApp.Comparator
+{
+    public class ObjComparator : IEqualityComparer<Obj>
+    {
+        public bool Equals(Obj x, Obj y)
+        {
+            return x.Name.Equals(y.Name);
+        }
+
+        public int GetHashCode(Obj obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
